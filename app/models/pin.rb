43d4ca1cb85cd9e8a9e5e4698d97c6257d187a24
Adrian McCarroll <nuadrian@gmail.com>
2014-12-04 16:57:54 +0100

    show user name

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,4 +2,6 @@ class Pin < ActiveRecord::Base
 	belongs_to :user
 
 	has_attached_file :image, :styles => { :large => " 800x800>", :medium => " 500x500>", :thumb => "100x100>" }
+	validates :image, presence: true
+	validates :description, presence: true
 end
